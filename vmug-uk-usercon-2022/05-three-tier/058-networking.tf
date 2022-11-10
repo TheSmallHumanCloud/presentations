a8a9eecddae44c1cce98935274f3987240275015
Stephan McTighe <65562694+smctighevcp@users.noreply.github.com>
2022-11-10 19:14:56 +0000

    Update 058-networking.tf

--- vmug-uk-usercon-2022/05-three-tier/058-networking.tf
+++ vmug-uk-usercon-2022/05-three-tier/058-networking.tf
@@ -1,7 +1,7 @@
 #NSX-T Segments
 resource "nsxt_policy_segment" "web-network" {
   display_name        = "${terraform.workspace}-${var.nsxt_networks[terraform.workspace].web.display_name}"
-  description         = var.nsxt_networks.web.description
+  description         = var.nsxt_networks[terraform.workspace].web.description
   connectivity_path   = data.nsxt_policy_tier1_gateway.t1.path
   transport_zone_path = data.nsxt_policy_transport_zone.overlay_tz.path
   subnet {
@@ -10,7 +10,7 @@ resource "nsxt_policy_segment" "web-network" {
 }
 resource "nsxt_policy_segment" "app-network" {
   display_name        = "${terraform.workspace}-${var.nsxt_networks[terraform.workspace].app.display_name}"
-  description         = var.nsxt_networks.app.description
+  description         = var.nsxt_networks[terraform.workspace].app.description
   connectivity_path   = data.nsxt_policy_tier1_gateway.t1.path
   transport_zone_path = data.nsxt_policy_transport_zone.overlay_tz.path
   subnet {
@@ -19,7 +19,7 @@ resource "nsxt_policy_segment" "app-network" {
 }
 resource "nsxt_policy_segment" "db-network" {
   display_name        = "${terraform.workspace}-${var.nsxt_networks[terraform.workspace].db.display_name}"
-  description         = var.nsxt_networks.db.description
+  description         = var.nsxt_networks[terraform.workspace].db.description
   connectivity_path   = data.nsxt_policy_tier1_gateway.t1.path
   transport_zone_path = data.nsxt_policy_transport_zone.overlay_tz.path
   subnet {
