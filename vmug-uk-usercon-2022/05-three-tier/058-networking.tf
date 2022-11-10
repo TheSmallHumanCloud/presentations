3bd1f0b0d82fe8514db6b66905fe02755dabea21
Stephan McTighe <65562694+smctighevcp@users.noreply.github.com>
2022-11-10 19:14:03 +0000

    Update 058-networking.tf

--- vmug-uk-usercon-2022/05-three-tier/058-networking.tf
+++ vmug-uk-usercon-2022/05-three-tier/058-networking.tf
@@ -1,6 +1,6 @@
 #NSX-T Segments
 resource "nsxt_policy_segment" "web-network" {
-  display_name        = "${terraform.workspace}-${var.nsxt_networks.web.display_name}"
+  display_name        = "${terraform.workspace}-${var.nsxt_networks[terraform.workspace].web.display_name}"
   description         = var.nsxt_networks.web.description
   connectivity_path   = data.nsxt_policy_tier1_gateway.t1.path
   transport_zone_path = data.nsxt_policy_transport_zone.overlay_tz.path
@@ -9,7 +9,7 @@ resource "nsxt_policy_segment" "web-network" {
   }
 }
 resource "nsxt_policy_segment" "app-network" {
-  display_name        = "${terraform.workspace}-${var.nsxt_networks.app.display_name}"
+  display_name        = "${terraform.workspace}-${var.nsxt_networks[terraform.workspace].app.display_name}"
   description         = var.nsxt_networks.app.description
   connectivity_path   = data.nsxt_policy_tier1_gateway.t1.path
   transport_zone_path = data.nsxt_policy_transport_zone.overlay_tz.path
@@ -18,7 +18,7 @@ resource "nsxt_policy_segment" "app-network" {
   }
 }
 resource "nsxt_policy_segment" "db-network" {
-  display_name        = "${terraform.workspace}-${var.nsxt_networks.db.display_name}"
+  display_name        = "${terraform.workspace}-${var.nsxt_networks[terraform.workspace].db.display_name}"
   description         = var.nsxt_networks.db.description
   connectivity_path   = data.nsxt_policy_tier1_gateway.t1.path
   transport_zone_path = data.nsxt_policy_transport_zone.overlay_tz.path
